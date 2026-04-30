be68e67c926025d3d71bf030f85f939a943abf8d
XLN <bjacnier28giler@gmail.com>
2026-04-30 11:02:31 -0500

    Agregar archivo de configuración

--- config.js
+++ config.js
@@ -1,16 +1,4 @@
-/**
- * config.example.js
- * 
- * 1. Copia este archivo y renómbralo a "config.js"
- * 2. Reemplaza los valores con las credenciales reales de tu proyecto de Supabase
- * 3. Asegúrate de añadir "config.js" a tu archivo .gitignore para no subir tus llaves al repositorio público.
- */
-
 const CONFIG = {
-    SUPABASE_URL: 'TU_SUPABASE_URL_AQUI',
-    SUPABASE_ANON_KEY: 'TU_SUPABASE_API_KEY_AQUI'
+    SUPABASE_URL: 'https://vtysnadgsfitusycrwfj.supabase.co/rest/v1/',
+    SUPABASE_ANON_KEY: 'sb_publishable_z3n-VTvVtmUwNHDacEw7fg_DG_e54CL'
 };
-
-// Nota: Si estuvieras usando un entorno con Node/Webpack/Vite, exportarías el objeto:
-// export default CONFIG;
-// En este caso, como lo cargamos directamente en el HTML, estará disponible globalmente.
